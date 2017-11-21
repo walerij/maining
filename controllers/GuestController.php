be6ce8f202980739c186079da3ea1dfc1ca278ec
walerij <becwal@yandex.ru>
2017-11-21 16:17:34 +0300

    Добавление контроллера Guest

---
+++ controllers/GuestController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+
+class GuestController extends Controller{
+    
+    
+    public function actionIndex()
+    {
+        return $this->render('index');
+        //return $this->render('about');
+    }
+}
---
+++ views/guest/index.php
@@ -0,0 +1,31 @@
+
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model app\models\LoginForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+use app\models;
+
+$this->title = 'Подписка';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<div class="panel panel-success" id="mainhead-border">
+    <div class="panel-heading" id="mainhead">
+        <i class="glyphicon glyphicon-bitcoin"></i>
+        &nbsp;&nbsp;
+        Подпишись на наши hash-ки
+    </div>
+    <div class="panel-body">
+       <?php $form = ActiveForm::begin(); ?>
+           
+
+           <?= $form->field($model, $attribute)); ?>
+
+           <?= $form->field( 'subject') ?>
+        <?php       ActiveForm::end(); ?>
+    </div>
+</div>
